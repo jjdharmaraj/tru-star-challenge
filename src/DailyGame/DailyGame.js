152eb7fac0416732f1a6d66d3307e42fdee650bf
Josiah Aviles <josiaha@uchicago.edu>
2021-06-04 18:26:15 -0500

    adding a few changes to daily challenge

--- src/DailyGame/DailyGame.js
+++ src/DailyGame/DailyGame.js
@@ -10,18 +10,19 @@ function DailyGame() {
           <h1>Pick a challenge!</h1>
           <div id="challenge1" class="dailychallenge">
             <h1>Challenge #1</h1>
+            <div id="videoplaceholder"></div>
             <p>Challenge 1 description goes here.</p>
-            <button id="challengeselect" onclick="">Select</button>
+            <button id="challengeselect" onclick="#">Select</button>
           </div>
           <div id="challenge2" class="dailychallenge">
             <h1>Challenge #2</h1>
             <p>Challenge 1 description goes here.</p>
-            <button id="challengeselect" onclick="">Select</button>
+            <button id="challengeselect" onclick="#">Select</button>
           </div>
           <div id="challenge3" class="dailychallenge">
             <h1>Challenge #3</h1>
             <p>Challenge 1 description goes here.</p>
-            <button id="challengeselect" onclick="">Select</button>
+            <button id="challengeselect" onclick="#">Select</button>
           </div>
         </div>
       </header>
