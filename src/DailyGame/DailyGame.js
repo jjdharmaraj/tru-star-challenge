2de4a0d1c99b01208e4ef22fc49748c69cdd956d
Josiah Aviles <josiaha@uchicago.edu>
2021-06-04 16:06:40 -0500

    created rough mockup of 3 daily challenges choice

--- src/DailyGame/DailyGame.js
+++ src/DailyGame/DailyGame.js
@@ -5,6 +5,19 @@ function DailyGame() {
     <div className="App">
       <header className="App-header">
         <p>Daily Game</p>
+        
+        <div id="challenges">
+          <h1>Pick a challenge!</h1>
+          <div id="dailychallenge">
+            <h1>Challenge #1</h1>
+          </div>
+          <div id="dailychallenge">
+            <h1>Challenge #1</h1>
+          </div>
+          <div id="dailychallenge">
+            <h1>Challenge #1</h1>
+          </div>
+        </div>
       </header>
     </div>
   );
